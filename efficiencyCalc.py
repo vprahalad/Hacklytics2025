cb28cd8cb09e5165e777d841327d6702502a9c41
lrajsankar <leelars25@gmail.com>
2025-02-22 18:45:01 -0500

    Update efficiencyCalc.py

--- efficiencyCalc.py
+++ efficiencyCalc.py
@@ -4,10 +4,10 @@ def efficiencyCalc(code_words, sentence):
     return efficiency
 def efficiencyWarning(efficiency):
     if efficiency < 0.2:
-        warning = "WARNING: This code has an efficiency score of less than 20% and 
+        warning = "WARNING: This code has an efficiency score of less than 20% and "
         "may have too many filler words in comparison to code words. Try again?"
     elif efficiency > 0.4:
-        warning = "WARNING: This code has an efficiency score of greater than 40%. 
+        warning = "WARNING: This code has an efficiency score of greater than 40%. "
         "This means that there may be too many code words and not enough filler words in the sentence. Try again?"
         
     
