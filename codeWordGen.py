64bbc18d3a96911323962e0f88c1f24c1d5ce06a
lrajsankar <leelars25@gmail.com>
2025-02-22 17:11:23 -0500

    Add files via upload

--- codeWordGen.py
+++ codeWordGen.py
@@ -4,19 +4,21 @@ from nltk.tokenize import word_tokenize
 from collections import Counter
 import numpy as np
 
-#nltk.download('punkt')
+nltk.download('punkt')
 
 def codeWordGen(csvFilepath):
     df = pd.read_csv(csvFilepath, usecols=[2])
     allWords = []
+    wordsInRange = []
     for sentence in df.iloc[:, 0].dropna():
         allWords.extend(word_tokenize(sentence.lower()))
     wordFreq = Counter(allWords)
     freqs = list(wordFreq.values())
     lowerBound = np.percentile(freqs, 30)
     upperBound = np.percentile(freqs, 60)
-    wordsInRange = [word for (word, freq) in wordFreq.items() if int(lowerBound) <= freq <= int(upperBound)]
-    #wordsInRange = [word for word, freq in wordFreq.items() if ]
+    for word, freq in wordFreq.items():
+        if freq >= lowerBound or freq <= upperBound:
+            wordsInRange.append(word)
     return len(wordsInRange)
 
 csvFilepath = "cleaned_data.csv"
